23c67d4462a266d04d73c36443a81c48c711a5d4
kylentennies <kylentennies@gmail.com>
2021-07-16 22:04:09 -0400

    painted ui

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,9 +7,13 @@ import { GithubService } from '../services/github.service';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  commits: Array<any>;
   projects: Array<any>;
+  repoName: any;
+  userName: string = "Netflix";
   
   constructor(private githubService:GithubService) {
+    this.commits = [];
     this.projects = [];
   }
 
@@ -18,9 +22,19 @@ export class ProjectsComponent implements OnInit {
   }
 
   getProjects() {
-    this.githubService.getProjects().subscribe((data) => {
-      console.log(data)
+    this.githubService.getProjects(this.userName).subscribe((data) => {
       this.projects = data;
+      console.log("repos: ", this.projects[0].name)
+      this.repoName = this.projects[0].name;
+      this.getCommits(this.userName, this.repoName);
     });
   }
+
+  getCommits(userName: string, repoName: string) {
+    this.githubService.getCommits(userName, repoName).subscribe((data) => {
+      console.log(this.repoName)
+      this.commits = data;
+    });
+  }
+  
 }
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http'
+import { HttpClient, HttpParams} from '@angular/common/http'
 import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+  baseUrl: string = "https://api.github.com/"
+  repoName!: string;
+  userName!: string;
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+  }
 
-  getProjects():Observable<any> {
-    const url = "https://api.github.com/orgs/netflix/repos";
-    return this.http.get<any>(url)
+  getProjects(userName: string):Observable<any> {
+    return this.http.get<any>(this.baseUrl + "orgs/" + userName + "/repos");
+  }
+
+  getCommits(userName: string, repoName: string):Observable<any> {
+    return this.http.get<any>(this.baseUrl + "repos/" + userName + "/" + repoName + "/commits");
   }
 }
