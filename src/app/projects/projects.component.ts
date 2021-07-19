6494e24a675969599b06220c07f765d6f2323cb0
kylentennies <kylentennies@gmail.com>
2021-07-18 21:32:02 -0400

    update to getProjects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GithubService } from '../services/github.service';
 
 @Component({
@@ -9,31 +9,35 @@ import { GithubService } from '../services/github.service';
 export class ProjectsComponent implements OnInit {
   commits: Array<any>;
   projects: Array<any>;
-  repoName: any;
+  repoName: string;
   userName: string = "Netflix";
-  
+  projectsArray: string[] = [];
+
   constructor(private githubService:GithubService) {
     this.commits = [];
     this.projects = [];
+    this.repoName = "";
   }
 
   ngOnInit(): void {
     this.getProjects();
   }
 
-  getProjects() {
+  async getProjects() {
     this.githubService.getProjects(this.userName).subscribe((data) => {
       this.projects = data;
-      console.log("repos: ", this.projects[0].name)
-      this.repoName = this.projects[0].name;
-      this.getCommits(this.userName, this.repoName);
+
+      for (let i = 0; i < this.projects.length; i++) {
+        this.repoName = this.projects[i].name;
+        this.getCommits(this.userName, this.repoName);
+      }
     });
   }
 
-  getCommits(userName: string, repoName: string) {
+  async getCommits(userName: string, repoName: string) {
     this.githubService.getCommits(userName, repoName).subscribe((data) => {
-      console.log(this.repoName)
       this.commits = data;
+      console.log(repoName, ":", this.commits)
     });
   }
   
